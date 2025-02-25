3829de78fd8796781e84200f851a791e3d969d77
Egor Kharitonov <ega.khar@gmail.com>
2025-02-24 23:24:10 -0500

    Made reviews into grid and responsive on mobile

--- src/Tutoring.jsx
+++ src/Tutoring.jsx
@@ -59,23 +59,33 @@ export default function Tutoring () {
 
             <p className="mb-5 font-semibold text-lg">Reviews about me:</p>
 
-            <div className="flex flex-col gap-[20px]">
-                <div className="flex flex-row text-sm gap-4">
-                    <div className="bg-zinc-800 p-4 rounded-2xl shadow-lg">
-                        <h2 className="mb-4 font-semibold text-lg">William</h2>
-                        <p>We met Egor “randomly” (so we thought).  We had our high schoolers in overpriced commercialized tutoring facilities that was not helping them much. Egor is incredibly smart and understanding. He will take his time and meet the student at their levels.  He is patient and very kind. If you need a math Tutor he is your guy. I thank God for allowing us to meet Egor “randomly”. Thanks Egor!</p>
-                    </div>
-                    <div className="bg-zinc-800 p-4 rounded-2xl shadow-lg">
-                        <h2 className="mb-4 font-semibold text-lg">Jane</h2>
-                        <p>My 9th-grade daughter was struggling with Algebra, but thanks to Egor, everything changed. He has a special way of connecting with teenagers, making even the most complicated topics understandable. His calm demeanor and sense of humor made learning enjoyable for her. Egor is an amazing tutor, and I can’t stop to recommend him enough to friends and anyone in need. </p>
-                    </div>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+
+                <div className="bg-zinc-800 p-4 rounded-2xl shadow-lg">
+                    <h2 className="mb-4 font-semibold text-lg">William</h2>
+                    <p>We met Egor “randomly” (so we thought). We had our high schoolers in overpriced commercialized
+                        tutoring facilities that was not helping them much. Egor is incredibly smart and understanding.
+                        He will take his time and meet the student at their levels. He is patient and very kind. If you
+                        need a math Tutor he is your guy. I thank God for allowing us to meet Egor “randomly”. Thanks
+                        Egor!</p>
                 </div>
-                <div className="flex flex-row text-sm gap-4">
-                    <div className="bg-zinc-800 p-4 rounded-2xl shadow-lg">
-                        <h2 className="mb-4 font-semibold text-lg">Artem</h2>
-                        <p>I finally understood math with this tutor. Math has always been difficult for me, especially on the SAT, but he helped me improve my score to (blank) and achieve a high result. We had tutoring sessions twice a week, and he created a personalized program to cover the gaps in what I missed during school lessons. He also provided a lot of practice assignments that were really helpful and hard to find anywhere else. I highly recommend him because, thanks to his help, I no longer feel afraid of math and was able to achieve a high score.</p>
-                    </div>
+                <div className="bg-zinc-800 p-4 rounded-2xl shadow-lg">
+                    <h2 className="mb-4 font-semibold text-lg">Jane</h2>
+                    <p>My 9th-grade daughter was struggling with Algebra, but thanks to Egor, everything changed. He has
+                        a special way of connecting with teenagers, making even the most complicated topics
+                        understandable. His calm demeanor and sense of humor made learning enjoyable for her. Egor is an
+                        amazing tutor, and I can’t stop to recommend him enough to friends and anyone in need. </p>
                 </div>
+                <div className="bg-zinc-800 p-4 rounded-2xl shadow-lg">
+                    <h2 className="mb-4 font-semibold text-lg">Artem</h2>
+                    <p>I finally understood math with this tutor. Math has always been difficult for me, especially on
+                        the SAT, but he helped me improve my score to (blank) and achieve a high result. We had tutoring
+                        sessions twice a week, and he created a personalized program to cover the gaps in what I missed
+                        during school lessons. He also provided a lot of practice assignments that were really helpful
+                        and hard to find anywhere else. I highly recommend him because, thanks to his help, I no longer
+                        feel afraid of math and was able to achieve a high score.</p>
+                </div>
+
 
             </div>
         </>
