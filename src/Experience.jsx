2e7b9d1ca41edbacd41ebd14d892acf8cb8e7e98
Egor Kharitonov <ega.khar@gmail.com>
2025-12-19 00:44:13 -0500

    feat: wip education/experience

---
+++ src/Experience.jsx
@@ -0,0 +1,10 @@
+export default function Experience() {
+    return (
+        <>
+        
+        <p>Stablein Solutions</p>
+                    <p>Software Engineer</p>
+                    <p>March 2025 - Now</p>
+        </>
+    )
+}
--- src/Home.jsx
+++ src/Home.jsx
@@ -3,22 +3,67 @@ import linkedin from "./assets/linkedin.png";
 import email from "./assets/email.png";
 import tg from "./assets/tg.png";
 import me from "./assets/me.png";
+import { useState } from "react";
 
+import Experience from "./Experience";
+import Education from "./Education";
 
 export default function Home () {
+
+    const [section, changesection] = useState("experience");
+
     return (
-        <div className="flex flex-col items-center justify-center h-full w-full">
+        <div className="flex flex-col h-full w-full">
 
-            <div className="grid justify-items-center gap-3">
+            <div className="flex flex-col md:flex-row justify-items-center gap-3">
 
-                    <img className="w-[150px] rounded-3xl shadow-md border-2 border-white" src={me} alt="photo of me"/>
+                <div>
+                    <img className="w-[150px] rounded-2xl shadow-md border-2 border-white" src={me} alt="photo of me"/>
+                </div>
 
-                    <div className="grid justify-items-center text-center ">
+                    <div className="flex flex-col gap-1 p-3 justify-center">
 
                         <h1 className="text-3xl font-semibold text-white sm:text-4xl sm:font-bold mb-2">Egor Kharitonov</h1>
-                        <h2 className="text-xl sm:text-xl">Software Engineer & Computer Science student</h2>
+                        <h2 className="text-xl sm:text-xl mb-2">Software Engineer & Computer Science student</h2>
 
                     </div>
+            </div>
+
+            <div className="mt-4 p-5 bg-slate-800 rounded-lg h-[200px]">
+                <div className="flex flex-col">
+
+                    <div className="flex flex-row">
+
+                        
+                    <div className="w-1/2 flex justify-end mr-16">
+                        <button className="border-[1px] border-white text-2xl font-semibold p-2 px-4  rounded-xl" onClick={() => changesection("experience")}>Experience</button>
+                    </div>
+
+                    <div className="w-1/2 flex justify-start">
+                        <button className="text-2xl font-semibold p-2 bg-slate-700 rounded-xl" onClick={() => changesection("education")}>Education</button>
+
+                    </div>
+
+                    </div>
+
+
+                    <div className="p-5 relative overflow-hidden">
+                    <div 
+                        key={section}
+                        className="animate-fade-in"
+                    >
+                        {
+                            section === "experience" ? <Experience/> : <Education/>
+                        }
+                    </div>
+                    </div>
+
+
+
+
+
+                </div>
+
             </div>
 
 
